7ae464ce06719946dd38bc02f928a096e9e4273d
erika kettleson <erikakettleson@gmail.com>
2020-10-31 12:59:41 -0700

    initial commit index connected, search working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'instantsearch.css/themes/reset.css';
+import 'instantsearch.css/themes/algolia.css';
+
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  RefinementList,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+
+
+const searchClient = algoliasearch(
+  'W71VYP4FK8',
+  '752be675b21917796189ff4560b7afbe'
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Search through the collections of the Harvard Art Museums.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <InstantSearch
+        indexName="Art"
+        searchClient={searchClient}
+        >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
     </div>
   );
 }
